d4a9e65e01734f88901bed3c3713b94fbcded067
Amaan <amaansayyad@yahoo.com>
2022-11-04 13:28:05 +0530

    updates

---
+++ frontend-main/src/utils/network-manager/adapters/adapter.types.ts
@@ -0,0 +1,45 @@
+import {
+  Network,
+  ChainName,
+  Profile,
+  Token,
+  Deposit,
+  Withdraw,
+  TokenSymbol,
+  DFPStrategy,
+  StrategiesByNetworks,
+} from '../manager.types';
+
+export type AdapterName = 'moralis' | 'web3' | 'ethers.js' | 'dfp' | 'none';
+
+export interface IAdapter {
+  get network(): Network;
+  get name(): AdapterName;
+  isAuthenticated(): boolean;
+  login(signMessage: string): Promise<Profile>;
+  logout(): Promise<void>;
+  getProfile(): Promise<Profile>;
+  getNativeToken(): Promise<Token>;
+  getTokens(): Promise<Token[]>;
+  switchNetwork(name: ChainName): Promise<void>;
+  getUsers(): Promise<number>;
+  getDeposits(): Promise<Deposit[]>;
+  getWithdrawals(): Promise<Withdraw[]>;
+  approveDeposit(amount: number, symbol: TokenSymbol): Promise<Deposit>;
+  // TODO change any
+  approveDepositAave(amount: number, symbol: TokenSymbol): Promise<any>;
+  depositAave(amount: number, symbol: TokenSymbol): Promise<any>;
+  withdrawAave(amount: number): Promise<any>;
+  getBalanceAave(): Promise<number>;
+
+  approveDepositUniswap(
+    amount: number,
+    symbol: TokenSymbol,
+    tokenNumber: number,
+  ): Promise<any>;
+  mintNewPosition(amount1: number, amount2: number): Promise<any>;
+
+  deposit(deposit: Deposit): Promise<Deposit>;
+  listStrategies(): Promise<DFPStrategy[]>;
+  listStrategiesByNetworks(): Promise<StrategiesByNetworks>;
+}
